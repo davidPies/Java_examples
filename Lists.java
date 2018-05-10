3afc45d9d5524faaf0a9b072f6deada21b2d2c18
davidPies <2014119@edtools.psd401.net>
2018-05-10 08:51:04 -0700

    Update Lists.java

--- Lists.java
+++ Lists.java
@@ -7,8 +7,11 @@ public class Lists {
 	public static List<String> Planets = new ArrayList<String>();
 	public Lists(){
 	}
-	public static addList(String s){
+	public static void addList(String s){ // essentially a command block
 		Planets.add(s);
+	}
+	public static String addList(String s){ // essentially a reporter block
+		return s;
 	}
 	public static void main(String[] args){
 		addList("name_1");
