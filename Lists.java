450f7d82f98c6f971d0ad369f98598557ea4dc0b
davidPies <2014119@edtools.psd401.net>
2018-05-10 09:02:36 -0700

    Update Lists.java

--- Lists.java
+++ Lists.java
@@ -4,21 +4,21 @@ import java.util.*;
 
 public class Lists {
 	
-	public static List<String> Planets = new ArrayList<String>(); // the list
+	public static List<String> Names = new ArrayList<String>(); // the list
 	
 	public static void addList(String s){ // essentially a command block
-		Planets.add(s);
+		Names.add(s);
 	}
 	public static String getListItems(int size){ // essentially a reporter block
-		return Planets.get(size);
+		return Names.get(size);
 	}
 	public static void main(String[] args){
 		addList("ethan");
 		addList("david");
-		System.out.println("length = "Planets.size()); // tells the list length
-		for(int planetListSize = 0; planetListSize < Planets.size(); planetListSize++){
-			System.out.println(planetListSize); // tells the the # of the item on the list (length is # of items minus 1)
-			System.out.println(getListItems(planetListSize)); // tells the names
+		System.out.println("length = " + Names.size()); // tells the list length
+		for(int nametListSize = 0; nametListSize < Names.size(); nametListSize++){
+			System.out.println("item " + nametListSize + " of list"); // tells the the # of the item on the list (length is # of items minus 1)
+			System.out.println(getListItems(nametListSize)); // tells the names
 		}
 	}
 }
