31f02b94be74e5964508feb224f1b81071698096
davidPies <2014119@edtools.psd401.net>
2018-05-10 08:49:40 -0700

    Update Lists.java

--- Lists.java
+++ Lists.java
@@ -5,11 +5,13 @@ import java.util.*;
 public class Lists {
 	
 	public static List<String> Planets = new ArrayList<String>();
-	public Lists(String s){
+	public Lists(){
+	}
+	public static addList(String s){
 		Planets.add(s);
 	}
 	public static void main(String[] args){
-		new Lists("name_1");
+		addList("name_1");
 		for(int planetListSize = 0; planetListSize < Planets.size(); planetListSize++)
 			System.out.println(planetListSize);
 	}
