b46619a6dad4400f526f0f0ed7ac1cefea38f891
davidPies <2014119@edtools.psd401.net>
2018-05-10 08:52:12 -0700

    Update Lists.java

--- Lists.java
+++ Lists.java
@@ -10,12 +10,13 @@ public class Lists {
 	public static void addList(String s){ // essentially a command block
 		Planets.add(s);
 	}
-	public static String addList(String s){ // essentially a reporter block
+	public static String addList_2(String s){ // essentially a reporter block
 		return s;
 	}
 	public static void main(String[] args){
 		addList("name_1");
 		for(int planetListSize = 0; planetListSize < Planets.size(); planetListSize++)
 			System.out.println(planetListSize);
+		System.out.println(addList_2("name_1"););
 	}
 }
