27fcc11a3d3fef8dc88bda6856d3152ed70fa9a2
davidPies <2014119@edtools.psd401.net>
2018-05-10 08:48:07 -0700

    Update Lists.java

--- Lists.java
+++ Lists.java
@@ -1,14 +1,15 @@
-package 
+package Java_examples;
 
 import java.util.*;
 
 public class Lists {
 	
-	public List<String> Planets = new ArrayList<String>();
-	public Lists(){
-		
+	public static List<String> Planets = new ArrayList<String>();
+	public Lists(String s){
+		Planets.add(s);
 	}
 	public static void main(String[] args){
+		new Lists("name_1");
 		for(int planetListSize = 0; planetListSize < Planets.size(); planetListSize++)
 			System.out.println(planetListSize);
 	}
