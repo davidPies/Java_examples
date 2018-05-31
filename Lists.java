a68ac3222331095b5da516d32e07baceec519d66
davidPies <2014119@edtools.psd401.net>
2018-05-31 09:07:11 -0700

    Update Lists.java

--- Lists.java
+++ Lists.java
@@ -1,20 +1,20 @@
 // package Java_examples; //not used for test compilation
 // use  https://www.compilejava.net/  for compilation
 
-import java.util.*;
+import java.util.*; // imports the sources
 
-public class Lists {
+public class Lists { // creates the file name
 	
-	public static List<String> Names = new ArrayList<String>(); // the list
+	public static List<String> Names = new ArrayList<String>(); // the list (length starts at 0)
 	
 	public static void addList(String s){ // essentially a command block
 		Names.add(s);
 	}
 	public static boolean checkSize(int size){ // essentially a predicate block
 		if(size < 0){
-		    return false;
+		    return false; // returns a false boolean if the list length is less than 0
 		}	
-		return true;
+		return true; // returns a true boolean otherwise of the previous boolean check
 	}
 	public static String getListItems(int size){ // essentially a reporter block
 		if(checkSize(size)){
@@ -23,9 +23,9 @@ public class Lists {
 			return null;
 		}
 	}
-	public static void main(String[] args){
-		addList("ethan");
-		addList("david");
+	public static void main(String[] args){ // public static void main(String[] args){} is where the code is detected by Java to be executed 
+		addList("ethan"); // adds an item to the list at position 0
+		addList("david"); // adds an item to the list at position 1
 		System.out.println("length = " + Names.size()); // tells the list length
 		for(int nametListSize = 0; nametListSize < Names.size(); nametListSize++){
 			System.out.println("item " + nametListSize + " of list"); // tells the the # of the item on the list
