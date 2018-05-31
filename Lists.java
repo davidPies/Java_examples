e6d69a0494491e23b0ebcc9bfee40a7c200e8313
davidPies <2014119@edtools.psd401.net>
2018-05-31 09:11:04 -0700

    Update Lists.java

--- Lists.java
+++ Lists.java
@@ -3,9 +3,9 @@
 
 import java.util.*; // imports the sources
 
-public class Lists { // creates the file name
+public class Lists { // creates the file name (brackets indicate a start and finish of classes and functions)
 	
-	public static List<String> Names = new ArrayList<String>(); // the list (length starts at 0)
+	public static List<String> Names = new ArrayList<String>(); // the list specified to words/numbers in quotation marks (length starts at 0)
 	
 	public static void addList(String s){ // essentially a command block
 		Names.add(s);
@@ -14,20 +14,20 @@ public class Lists { // creates the file name
 		if(size < 0){
 		    return false; // returns a false boolean if the list length is less than 0
 		}	
-		return true; // returns a true boolean otherwise of the previous boolean check
+		return true; // returns a true boolean if otherwise of the previous boolean check
 	}
 	public static String getListItems(int size){ // essentially a reporter block
-		if(checkSize(size)){
-		    return Names.get(size);
+		if(checkSize(size)){ // implements the predicate block
+		    return Names.get(size); // returns the value of the name at the specified item number (0 to 1)
 		}else{
-			return null;
+			return null; // returns nothing if otherwise of the previous statement
 		}
 	}
 	public static void main(String[] args){ // public static void main(String[] args){} is where the code is detected by Java to be executed 
 		addList("ethan"); // adds an item to the list at position 0
 		addList("david"); // adds an item to the list at position 1
 		System.out.println("length = " + Names.size()); // tells the list length
-		for(int nametListSize = 0; nametListSize < Names.size(); nametListSize++){
+		for(int nametListSize = 0; nametListSize < Names.size(); nametListSize++){ // esentially a repeat, for the length of the list
 			System.out.println("item " + nametListSize + " of list"); // tells the the # of the item on the list
 			System.out.println(getListItems(nametListSize)); // tells the names
 
